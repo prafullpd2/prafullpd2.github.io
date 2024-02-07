ac67f7a6121ae888a208a36033fc2859ee95bf97
Prafull Dhadkar Personal <prafull.dhadkar@gmail.com>
2024-02-07 02:01:23 -0600

    second angular build

---
+++ chunk-INCICMIA.js
@@ -0,0 +1,1 @@
+import{Ca as i,Da as a,La as r,Pa as c,V as o}from"./chunk-JKTX7OYB.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-connect"]],standalone:!0,features:[c],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"connect works!"),a())}});let t=e;return t})();export{d as ConnectComponent};
